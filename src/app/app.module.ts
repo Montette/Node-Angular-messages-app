744b10dc978a3c53726a4e0de5c04ec842912020
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2019-10-26 15:26:18 +0200

    change form for reactive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule  } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule, MatProgressSpinnerModule } from '@angular/material';
 import { AppComponent } from './app.component';
@@ -25,7 +25,7 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule ,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
@@ -16,12 +16,21 @@ export class PostCreateComponent implements OnInit {
   // enteredTitle = '';
   private mode = 'create';
   private postId: string;
+  form: FormGroup;
   post: Post;
   showSpinner: boolean = false;
 
   constructor(public postsService: PostsService, public route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)]
+      }),
+      content: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(10)]
+      })
+    })
     this.route.paramMap.subscribe((paramMap: ParamMap)=> {
       console.log(paramMap);
       if(paramMap.has('postId')) {
@@ -30,7 +39,11 @@ export class PostCreateComponent implements OnInit {
         this.showSpinner = true;
         this.postsService.getPost(this.postId).subscribe(data => {
           this.showSpinner = false;
-          this.post = {id: data._id, title: data.title, content: data.content}
+          this.post = {id: data._id, title: data.title, content: data.content};
+          this.form.setValue({
+            'title': this.post.title,
+            'content': this.post.content
+          })
         })
 
       } else {
@@ -41,17 +54,17 @@ export class PostCreateComponent implements OnInit {
     });
   }
 
-  onAddPost(form: NgForm) {
-    if(form.invalid) {
+  onAddPost() {
+    if(this.form.invalid) {
       return;
     }
     if(this.postId) {
-      this.postsService.editPost(form.value.title, form.value.content, this.postId);
+      this.postsService.editPost(this.form.value.title, this.form.value.content, this.postId);
     } else {
-      this.postsService.addPost(form.value.title, form.value.content);
+      this.postsService.addPost(this.form.value.title, this.form.value.content);
     }
 
-  form.resetForm();
+  this.form.reset();
 
   }
 
